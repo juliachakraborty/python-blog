4e8d7916420f8116b33ab1bb8d2673dea21692f1
Julia Chakraborty <juliachakraborty@MacBook-Pro.local>
2024-12-16 10:35:50 +1100

    remove output dir

--- blog_app.py
+++ blog_app.py
@@ -2,11 +2,10 @@ from jinja2 import Environment, FileSystemLoader
 import os
 
 # Paths
-templatesS_DIR = 'templates'
-OUTPUT_DIR = 'output'
+TEMPLATES_DIR = 'templates'
 
 # Set up Jinja2 environment
-env = Environment(loader=FileSystemLoader(templatesS_DIR))
+env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
 # List of pages to generate
 pages = [
@@ -14,14 +13,11 @@ pages = [
     {"templates": "about.html", "output": "about.html", "context": {"title": "About Me"}},
 ]
 
-# Generate HTML files
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
 for page in pages:
     template = env.get_template(page["templates"])
     output = template.render(page["context"])
     
-    output_path = os.path.join(OUTPUT_DIR, page["output"])
+    output_path = os.path.join(os.getcwd(), page["output"])
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(output)
         print(f"Generated static file: {output_path}")
